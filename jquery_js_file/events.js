bbe9c677ad12d6100d33f4e2c986edb0e0e90881
Developer Baboo <babooheerani@gmail.com>
2023-11-30 05:11:32 +0500

    added descriptions of each functions

--- jquery_js_file/events.js
+++ jquery_js_file/events.js
@@ -1,14 +1,15 @@
 $(document).ready(function () {
+    // When foucs on element that function triggers
     $('#sname, #sclass, #scountry').focus(function () {
         $(this).css('background-color', 'lime');
     });
 
-
+ // opposite of focus
     $('#sname, #sclass, #scountry').blur(function () {
         $(this).css('background-color', '');
     });
 
-
+// work with select box
     $('#scountry').change(function () {
         // $(this).css('background-color', 'pink');
 
@@ -17,7 +18,7 @@ $(document).ready(function () {
     });
 
 
-
+//When we select whatever written in input box that event triggers
     $('#sname, #sclass').select(function () {
         $(this).css('background-color', 'yellow');
     });

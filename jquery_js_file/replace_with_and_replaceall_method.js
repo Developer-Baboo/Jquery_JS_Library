180bec73c82424d452e9be65751836c9d9be6fa7
Developer Baboo <babooheerani@gmail.com>
2023-12-02 20:12:57 +0500

    Replacewith and replaceall method

---
+++ jquery_js_file/replace_with_and_replaceall_method.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    console.log("Jquery Working");
+    $('#replaceBtn').click(function () {
+        // $('#box p:first').replaceWith("<h1>Replaced Value</h1>")
+        //$('#box p:nth-child(3)').replaceWith("<h1>Replaced Value</h1>")
+        // $("<h3>Jquery Methods</h3>").replaceAll('#box p');
+        // $('#box p').replaceWith("<h1>Replaced Value</h1>")
+    });
+});
